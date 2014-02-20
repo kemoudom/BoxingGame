8bab4a1e7561425f326a71349aeb218da7cf1a7e
kemoudom <kemoudom@gmail.com>
2014-02-20 09:22:17 +0100

    GUI

--- src/com/challenge/boxinggame/MainActivity.java
+++ src/com/challenge/boxinggame/MainActivity.java
@@ -1,15 +1,82 @@
 package com.challenge.boxinggame;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
+
+	Button buttonLeft, buttonRight, leftSideLeftHand, leftSideRightHand,
+			rightSideLeftHand, rightSideRightHand;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		buttonLeft = (Button) findViewById(R.id.left_punch);
+		buttonRight = (Button) findViewById(R.id.right_punch);
+		leftSideLeftHand = (Button) findViewById(R.id.left_left);
+		leftSideRightHand = (Button) findViewById(R.id.left_right);
+		rightSideLeftHand = (Button) findViewById(R.id.right_left);
+		rightSideRightHand = (Button) findViewById(R.id.right_right);
+
+		buttonLeft.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+
+			}
+		});
+
+		buttonRight.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+
+			}
+		});
+
+		leftSideLeftHand.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+
+			}
+		});
+
+		leftSideRightHand.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+
+			}
+		});
+
+		rightSideLeftHand.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+
+			}
+		});
+
+		rightSideRightHand.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+
+			}
+		});
 	}
 
 	@Override
