a0172785f64e2c15ff892af75ca06169e7bdb622
Robin <bussenot.robin@gmail.com>
2014-02-20 14:16:32 +0100

    add setupActivity

--- gen/com/challenge/boxinggame/R.java
+++ gen/com/challenge/boxinggame/R.java
@@ -8,6 +8,12 @@
 package com.challenge.boxinggame;
 
 public final class R {
+    public static final class anim {
+        public static final int anim_transalte_head_left=0x7f040000;
+        public static final int anim_transalte_head_right=0x7f040001;
+        public static final int anim_translate=0x7f040002;
+        public static final int right_anim_translate=0x7f040003;
+    }
     public static final class attr {
     }
     public static final class dimen {
@@ -17,33 +23,53 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
-        public static final int left_right=0x7f020001;
-        public static final int right_left=0x7f020002;
+        public static final int head=0x7f020000;
+        public static final int head1=0x7f020001;
+        public static final int horizontal=0x7f020002;
+        public static final int ic_launcher=0x7f020003;
+        public static final int key_a=0x7f020004;
+        public static final int key_b=0x7f020005;
+        public static final int left_right=0x7f020006;
+        public static final int right_left=0x7f020007;
+        public static final int vertical=0x7f020008;
     }
     public static final class id {
-        public static final int action_settings=0x7f080006;
-        public static final int left_left=0x7f080003;
-        public static final int left_punch=0x7f080001;
-        public static final int left_right=0x7f080002;
-        public static final int right_left=0x7f080004;
-        public static final int right_punch=0x7f080000;
-        public static final int right_right=0x7f080005;
+        public static final int BoxingGame=0x7f09000d;
+        public static final int action_settings=0x7f090011;
+        public static final int connection=0x7f09000e;
+        public static final int container=0x7f090000;
+        public static final int exit=0x7f090010;
+        public static final int head_left=0x7f090007;
+        public static final int head_right=0x7f090008;
+        public static final int image=0x7f09000c;
+        public static final int left_left=0x7f090004;
+        public static final int left_punch=0x7f090002;
+        public static final int left_right=0x7f090003;
+        public static final int right_left=0x7f090005;
+        public static final int right_punch=0x7f090001;
+        public static final int right_right=0x7f090006;
+        public static final int start=0x7f09000f;
+        public static final int x_axis=0x7f090009;
+        public static final int y_axis=0x7f09000a;
+        public static final int z_axis=0x7f09000b;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int main=0x7f030001;
+        public static final int setup=0x7f030002;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int main=0x7f080000;
+        public static final int setup=0x7f080001;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
-        public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
+        public static final int action_settings=0x7f060001;
+        public static final int app_name=0x7f060000;
+        public static final int title_activity_setup=0x7f060002;
     }
     public static final class style {
         /** 
@@ -67,10 +93,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
